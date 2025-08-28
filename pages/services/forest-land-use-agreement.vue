8d477c1cdccb223ca934eb2ca0ebb42fe40c5791
fatmatawfeek <fatmatawfeek97@gmail.com>
2025-08-28 16:57:01 +0300

    create flag page

---
+++ pages/services/forest-land-use-agreement.vue
@@ -0,0 +1,151 @@
+<template>
+    <!-- hero section  -->
+    <section
+        class="bg-[url('/assets/images/main-imgs/services/application-img.jpg')] bg-cover bg-left md:bg-top min-h-dvh py-20 w-full px-10 md:px-20 flex items-center"
+        id="top"
+    >
+        <div class="md:max-w-[45%] text-white">
+            <!-- text 1 -->
+            <h1 class="text-2xl md:text-5xl 4k:text-6xl mb-5">Forest Land Use Agreement (FLAg)</h1>
+            <p class="text-lg 4k:text-3xl">
+                The DENR Forest Land Use Agreement allows temporary use of forestlands for specific
+                purposes, ensuring sustainable management and conservation practices.
+            </p>
+            <div class="flex gap-2 mt-5 w-full flex-wrap">
+                <NuxtLink
+                    to="#tsd-new"
+                    class="4k:text-2xl border-2 border-white transition-all duration-200 text-white py-2 px-5 rounded-lg uppercase"
+                >
+                    New
+                </NuxtLink>
+            </div>
+        </div>
+    </section>
+    <!-- body  -->
+    <section class="py-10 md:py-20">
+        <div class="container">
+            <!-- section 1  -->
+            <section id="tsd-new" class="py-10">
+                <h2 class="text-2xl md:text-4xl font-regular text-center mb-5 uppercase">
+                    Chemical Control Order (CCO) Registration Application
+                </h2>
+
+                <h4
+                    class="uppercase bg-[#547326] py-1 text-xl md:text-2xl 4k:text-3xl text-center rounded text-white font-semibold mb-10"
+                >
+                    scope OF WORK - NEW
+                </h4>
+                <h5 class="text-lg md:text-xl mb-5 font-semibold">
+                    RESPONSIBILITIES OF THE CONSULTANT
+                </h5>
+                <ul class="4k:text-2xl list-disc list-inside mb-10 ml-[19px]">
+                    <li class="mb-2">Notary of Duly Accomplished Application Form</li>
+                    <li class="mb-2">Securing BIR Certification on the zonal valuation</li>
+                    <li class="mb-2">
+                        Processing CENRO Certification on Land Classification (if unavailable)
+                    </li>
+                    <li class="mb-2">
+                        Drafting of Letter of Request for LGU Endorsement (if unavailable)
+                    </li>
+                    <li class="mb-2">
+                        Drafting of Letter of Request for issuance of Tourism Development Plan from
+                        LGU
+                    </li>
+                    <li class="mb-2">
+                        Drafting of Comprehensive Development and Management Plan (CDMP)
+                    </li>
+                    <li class="mb-2">
+                        Processing Application to secure Certificate of Non-overlap from NCIP
+                    </li>
+                </ul>
+
+                <h4
+                    class="uppercase bg-[#547326] py-1 text-xl md:text-2xl 4k:text-3xl text-center rounded text-white font-semibold mb-10"
+                >
+                    REQUIREMENTS - nEW
+                </h4>
+                <h5 class="text-lg md:text-xl mb-10 font-semibold">REQUIREMENT FROM PROPONENT</h5>
+                <ul class="4k:text-2xl list-disc list-inside mb-10 ml-[19px]">
+                    <li class="mb-2">Filling out the Application Form</li>
+                    <li class="mb-2">Payment of Application Fee</li>
+                    <li class="mb-2">
+                        Provision of Approved Survey Plan with Technical Description and duly signed
+                        by a Geodetic Engineer
+                    </li>
+                    <li class="mb-2">
+                        Provision of CENRO Certification on Land Classification (if available)
+                    </li>
+                    <li class="mb-2">Provision of Proof of Identity of the Leasee</li>
+                    <ul class="4k:text-2xl list-disc list-inside mb-2 ml-[19px]">
+                        <li class="mb-2">
+                            Individual – Philippine Statistics Authority (PSA)-certified copy of
+                            Birth Certificate
+                        </li>
+                        <li class="mb-2">
+                            Naturalized Filipino citizen – Philippine Statistics
+                            Authority (PSA)-certified copy of Certificate of Naturalization
+                        </li>
+                        <li class="mb-2">
+                            Corporation/Association/Partnership and the like with at least sixty
+                            percent (60%) of the capital owned by Filipino citizen/s:
+                        </li>
+                        <ul class="4k:text-2xl list-disc list-inside mb-2 ml-[19px]">
+                            <li class="mb-2">
+                                a. Certified true copy of the Securities and Exchange Commission
+                                (SEC) Registration Certificate
+                            </li>
+                            <li class="mb-2">
+                                b. Certified true copy of Articles of Incorporation
+                            </li>
+                            <li class="mb-2">c. Certified true copy of By-Laws</li>
+                            <li class="mb-2">
+                                d. Certified true copy of the Board resolution/Secretary’s
+                                Certificate of the corporate governing body (Board of Directors,
+                                Board of Trustees, etc.) designating the authorized representative
+                                of said corporation, association, or partnership to apply/sign
+                                documents for and on behalf of the company
+                            </li>
+                        </ul>
+                        <li class="mb-2">
+                            The applicant uses a name, style, or trade name, other than the true
+                            name, certified true copy of the certification of registration of such
+                            name, style or trade name with the Department of Trade and Industry
+                            (DTI) and SEC
+                        </li>
+                        <li class="mb-2">
+                            Cooperative - Certified true copy of Certificate of Registration with
+                            the Cooperative Development Authority (CDA)
+                        </li>
+                    </ul>
+                    <li class="mb-2">
+                        Provision of Proof of Financial Capability - Bank Certification
+                    </li>
+                    <li class="mb-2">LGU Endorsement (if available)</li>
+                </ul>
+
+                <div class="flex flex-col gap-3 items-start justify-center">
+                    <NuxtLink
+                        to="#"
+                        class="flex-1 4k:text-2xl bg-[#547326] transition-all duration-200 text-white py-2 px-5 rounded"
+                    >
+                        Download PDF
+                    </NuxtLink>
+                    <NuxtLink
+                        to="#top"
+                        class="flex-1 4k:text-2xl border-2 border-[#547326] transition-all duration-200 text-[#547326] py-2 px-5 rounded"
+                    >
+                        Back to top
+                    </NuxtLink>
+                </div>
+            </section>
+        </div>
+    </section>
+</template>
+
+<script setup>
+useHead({
+    title: "Forest Land Use Agreement (FLAg) - Envi-Comm Corporation",
+});
+</script>
+
+<style lang="scss" scoped></style>
