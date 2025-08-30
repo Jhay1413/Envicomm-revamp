7789e7a8bdd73b439a31420bcb852418fe0f37e2
fatmatawfeek <fatmatawfeek97@gmail.com>
2025-08-30 09:33:51 +0300

    create ac page

---
+++ pages/services/area-clearance.vue
@@ -0,0 +1,182 @@
+<template>
+    <!-- hero section  -->
+    <section
+        class="bg-[url('/assets/images/main-imgs/services/application-img.jpg')] bg-cover bg-left md:bg-top min-h-dvh py-20 w-full px-10 md:px-20 flex items-center"
+        id="top"
+    >
+        <div class="md:max-w-[45%] text-white">
+            <!-- text 1 -->
+            <h1 class="text-2xl md:text-5xl 4k:text-6xl mb-5">Area Clearance (AC)</h1>
+            <p class="text-lg 4k:text-3xl">
+                The DENR Area Clearance is for projects requiring land use conversion or occupancy
+                in forestlands/protected areas, ensuring environmental compatibility.
+            </p>
+            <div class="flex gap-2 mt-5 w-full flex-wrap">
+                <NuxtLink
+                    to="#tsd-new"
+                    class="4k:text-2xl border-2 border-white transition-all duration-200 text-white py-2 px-5 rounded-lg uppercase"
+                >
+                    New
+                </NuxtLink>
+            </div>
+        </div>
+    </section>
+    <!-- body  -->
+    <section class="py-10 md:py-20">
+        <div class="container">
+            <!-- section 1  -->
+            <section id="tsd-new" class="py-10">
+                <h2 class="text-2xl md:text-4xl font-regular text-center mb-5 uppercase">
+                    Area Clearance (AC)
+                </h2>
+
+                <h4
+                    class="uppercase bg-[#547326] py-1 text-xl md:text-2xl 4k:text-3xl text-center rounded text-white font-semibold mb-10"
+                >
+                    scope OF WORK - NEW
+                </h4>
+                <h5 class="text-lg md:text-xl mb-5 font-semibold">
+                    RESPONSIBILITIES OF THE CONSULTANT
+                </h5>
+                <ul class="4k:text-2xl list-disc list-inside mb-10 ml-[19px]">
+                    <li class="mb-2">Conduct preliminary site assessment and due diligence</li>
+                    <li class="mb-2">
+                        Prepare and consolidate all necessary documentary requirements for the
+                        application
+                    </li>
+                    <li class="mb-2">
+                        Facilitate the submission of the application to the appropriate DENR office
+                        (e.g., Land Management Bureau or Regional Office)
+                    </li>
+                    <li class="mb-2">
+                        Coordinate with government agencies for any required certifications or
+                        endorsements
+                    </li>
+                    <li class="mb-2">
+                        Assist in the preparation of a site development plan or sketch plan
+                    </li>
+                    <li class="mb-2">
+                        Respond to queries and requests for additional information from the DENR
+                    </li>
+                    <li class="mb-2">
+                        Represent the Proponent during the application and evaluation process
+                    </li>
+                </ul>
+                <h4
+                    class="uppercase bg-[#547326] py-1 text-xl md:text-2xl 4k:text-3xl text-center rounded text-white font-semibold mb-10"
+                >
+                    REQUIREMENTS - nEW
+                </h4>
+                <h5 class="text-lg md:text-xl mb-10 font-semibold">REQUIREMENT FROM PROPONENT</h5>
+                <h6 class="mb-2 uppercase">For Proposed Reclamation Projects</h6>
+                <ul class="4k:text-2xl list-disc list-inside mb-10 ml-[19px]">
+                    <li class="mb-2">Project Description, to include the following:</li>
+                    <ul class="4k:text-2xl list-disc list-inside mb-2 ml-[19px]">
+                        <li class="mb-2">Sketch Plan</li>
+                        <li class="mb-2">
+                            Indicative Site Development Plan to state the usage/purpose of the area,
+                            including buffer zone, mini-park development, roadside tree
+                        </li>
+                        <li class="mb-2">
+                            Description of the Reclamation Site – average depth, boundaries,
+                            immediate vicinity, distance from shoreline, existing road/access/egress
+                            and ingress, available infrastructure facilities, utilities, e.g.,
+                            source of power, water and telecommunication, 5-year LGU infrastructure
+                            program which will enhance viability of proposed reclamation
+                        </li>
+                        <li class="mb-2">Valid Sources of Fill Materials</li>
+                        <li class="mb-2">
+                            Reclamation Methodology – the procedure/technique in undertaking the
+                            activity to include the estimate volume of fill materials, type of
+                            materials and sources, containment/retention wall and consolidation of
+                            materials
+                        </li>
+                        <li class="mb-2">
+                            Estimated cost of reclamation and land development including data (i.e.,
+                            existing labor force, structure and average cost and available equipment
+                            and average cost/rental rates)
+                        </li>
+                        <li class="mb-2">Proposed funding/financing of the project</li>
+                        <li class="mb-2">
+                            Proposed project timetable – reclamation, land development and other
+                            related activities
+                        </li>
+                        <li class="mb-2">
+                            Prevailing market land values of types/uses similar to the proposed land
+                            use/s within immediate vicinity duly certified by the local assessor/s
+                            and based on Bureau of Internal Revenue (BIR) zonal valuations
+                        </li>
+                        <li class="mb-2">
+                            Other documents, data and information pertinent to the proposed
+                            reclamation
+                        </li>
+                    </ul>
+                    <li class="mb-2">
+                        Geotagged photos showing the panoramic view of the land and adjoining areas,
+                        including existing landmarks and identifying features
+                    </li>
+                    <li class="mb-2">
+                        Sangguniang Resolution/s (Municipality/City, Province) authorizing the
+                        application for reclamation in case of LGU, or Board of Directors’
+                        Resolution in case of Government corporation. In case the applicant is the
+                        provincial LGU, the Sangguniang Resolution of the Municipality/City where
+                        the proposed relocation is located shall be secured in addition to the
+                        Provincial Board Resolution
+                    </li>
+                    <li class="mb-2">
+                        Clearances from concerned government agencies such as PRA (in case applicant
+                        is other than PRA), PPA (in areas under its jurisdiction), DOT (in areas
+                        declared as tourist zones), BFAR, DOE (for submarine pipelines and power
+                        cable connections), concerned LGU (if the area is within municipal waters
+                        and the project is not LGU-initiated, through a Sangguniang Resolution),
+                        DPWH and others whenever necessary
+                    </li>
+                    <li class="mb-2">Geohazard Identification Report (GIR) of the area</li>
+                </ul>
+                <h6 class="mb-2 uppercase">Over Existing Reclaimed Lands</h6>
+                <ul class="4k:text-2xl list-disc list-inside mb-10 ml-[19px]">
+                    <li class="mb-2">Approved Survey Plan with technical description</li>
+                    <li class="mb-2">
+                        Geotagged photos showing the panoramic view of the reclaimed land
+                    </li>
+                    <li class="mb-2">Clearances from other concerned government agencies</li>
+                    <li class="mb-2">
+                        Certification by the CENRO/Implementing PENRO/Region as the case may be
+                        regarding the status of the area and the land classification of the adjacent
+                        land
+                    </li>
+                    <li class="mb-2">Copy of the EIS/ECC as the case may be</li>
+                    <li class="mb-2">
+                        Sangguniang Resolution/s endorsing the issuance of Proclamation / Special
+                        Patent in case of LGU, or Board Directors’ Resolution in case of Government
+                        corporation
+                    </li>
+                    <li class="mb-2">Narrative history of the reclamation</li>
+                </ul>
+
+                <div class="flex flex-col gap-3 items-start justify-center">
+                    <NuxtLink
+                        to="#"
+                        class="flex-1 4k:text-2xl bg-[#547326] transition-all duration-200 text-white py-2 px-5 rounded"
+                    >
+                        Download PDF
+                    </NuxtLink>
+                    <NuxtLink
+                        to="#top"
+                        class="flex-1 4k:text-2xl border-2 border-[#547326] transition-all duration-200 text-[#547326] py-2 px-5 rounded"
+                    >
+                        Back to top
+                    </NuxtLink>
+                </div>
+            </section>
+        </div>
+    </section>
+</template>
+
+<script setup>
+useHead({
+    title: "Area Clearance (AC) - Envi-Comm Corporation",
+});
+</script>
+
+<style lang="scss" scoped></style>
