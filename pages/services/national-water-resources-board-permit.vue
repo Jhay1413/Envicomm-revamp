249f80d8a347c7d9df94d11392dde49c19a11a4b
fatmatawfeek <fatmatawfeek97@gmail.com>
2025-08-30 12:43:32 +0300

    create nwrb page

---
+++ pages/services/national-water-resources-board-permit.vue
@@ -0,0 +1,324 @@
+<template>
+    <!-- hero section  -->
+    <section
+        class="bg-[url('/assets/images/main-imgs/services/application-img.jpg')] bg-cover bg-left md:bg-top min-h-dvh py-20 w-full px-10 md:px-20 flex items-center"
+        id="top"
+    >
+        <div class="md:max-w-[45%] text-white">
+            <!-- text 1 -->
+            <h1 class="text-2xl md:text-5xl 4k:text-6xl mb-5">
+                National Water Resources Board (NWRB) Permit
+            </h1>
+            <p class="text-lg 4k:text-3xl">
+                The NWRB Clearance (or Water Permit) is for obtaining rights to appropriate and use
+                water from any source, ensuring sustainable water resource management.
+            </p>
+            <div class="flex gap-2 mt-5 w-full flex-wrap">
+                <NuxtLink
+                    to="#tsd-new"
+                    class="4k:text-2xl border-2 border-white transition-all duration-200 text-white py-2 px-5 rounded-lg uppercase"
+                >
+                    New
+                </NuxtLink>
+            </div>
+        </div>
+    </section>
+    <!-- body  -->
+    <section class="py-10 md:py-20">
+        <div class="container">
+            <!-- section 1  -->
+            <section id="tsd-new" class="py-10">
+                <h2 class="text-2xl md:text-4xl font-regular text-center mb-5 uppercase">
+                    National Water Resources Board (NWRB) Permit
+                </h2>
+
+                <h4
+                    class="uppercase bg-[#547326] py-1 text-xl md:text-2xl 4k:text-3xl text-center rounded text-white font-semibold mb-10"
+                >
+                    scope OF WORK - NEW
+                </h4>
+                <h5 class="text-lg md:text-xl mb-5 font-semibold">
+                    RESPONSIBILITIES OF THE CONSULTANT
+                </h5>
+                <ul class="4k:text-2xl list-disc list-inside mb-10 ml-[19px]">
+                    <li class="mb-2">
+                        Conduct a thorough review of the project and its water requirements to
+                        determine the appropriate type of Water Permit
+                    </li>
+                    <li class="mb-2">
+                        Assist the proponent in securing and preparing all necessary legal,
+                        corporate, and technical documents
+                    </li>
+                    <li class="mb-2">
+                        Coordinate with relevant government agencies (e.g., DENR, LWUA, LGU, DOE) to
+                        obtain required certifications and clearances
+                    </li>
+                    <li class="mb-2">
+                        Prepare or facilitate the preparation of technical plans and data, such as
+                        Vicinity Maps and Well Drilling Data
+                    </li>
+                    <li class="mb-2">
+                        Consolidate the complete application package and submit it to the NWRB
+                    </li>
+                    <li class="mb-2">
+                        Act as the liaison between the proponent and the NWRB, responding to queries
+                        and facilitating compliance with any additional requirements
+                    </li>
+                    <li class="mb-2">
+                        Provide support throughout the permit processing and approval stages
+                    </li>
+                </ul>
+
+                <h4
+                    class="uppercase bg-[#547326] py-1 text-xl md:text-2xl 4k:text-3xl text-center rounded text-white font-semibold mb-10"
+                >
+                    REQUIREMENTS - nEW
+                </h4>
+                <h5 class="text-lg md:text-xl mb-10 font-semibold">REQUIREMENT FROM PROPONENT</h5>
+                <h6 class="mb-2 uppercase">WATER PERMIT FOR MUNICIPAL USE</h6>
+                <ul class="4k:text-2xl list-disc list-inside mb-10 ml-[19px]">
+                    <li>
+                        Proof of land ownership or legal right to use the property where the water
+                        source is situated
+                    </li>
+                    <li>
+                        Certificate of Registration from DTI, CDA, or SEC, with Articles of
+                        Incorporation and Corporate Secretary Certification on capital structure
+                    </li>
+                    <li>Certificate of Conformance from LWUA (for Water Districts only)</li>
+                    <li>
+                        Certificate of Registration (if a Barangay Waterworks or Rural Waterworks
+                        and Sanitation Association)
+                    </li>
+                    <li>
+                        Vicinity Map/Location Plan (scale 1:10,000 or 1:50,000) showing the exact
+                        location of the point of diversion
+                    </li>
+                    <li>Subdivision Plan (if applicable)</li>
+                    <li>
+                        Well Drilling Data (for existing groundwater sources), authenticated by the
+                        well driller, including Physical and Chemical analysis of water
+                    </li>
+                    <li>
+                        Sangguniang Bayan / Regional Development Council endorsement (for
+                        LGU-managed water supply facilities)
+                    </li>
+                    <li>
+                        Environmental Compliance Certificate (ECC) or Certificate of Non-Coverage
+                        (CNC) from DENR-Regional Office
+                    </li>
+                    <li>Other documents that may be required by the Board</li>
+                </ul>
+
+                <h6 class="mb-2 uppercase">WATER PERMIT FOR IRRIGATION USE</h6>
+                <ul class="4k:text-2xl list-disc list-inside mb-10 ml-[19px]">
+                    <li>
+                        Proof of land ownership or legal right to use the property where the water
+                        source is situated
+                    </li>
+                    <li>
+                        Certificate of Registration from DTI, CDA, or SEC, with Articles of
+                        Incorporation and Corporate Secretary Certification on capital structure
+                    </li>
+                    <li>
+                        Vicinity Map/Location Plan (scale 1:50,000) showing the exact location of
+                        the point of diversion
+                    </li>
+                    <li>
+                        General layout of the system, delineating the area for water use, adjoining
+                        lands, and owners relative to the point of diversion
+                    </li>
+                    <li>Well Drilling Data (for existing groundwater sources)</li>
+                    <li>
+                        Environmental Compliance Certificate (ECC) or Certificate of Non-Coverage
+                        (CNC) from DENR-Regional Office
+                    </li>
+                    <li>Other documents that may be required by the Board</li>
+                </ul>
+
+                <h6 class="mb-2 uppercase">WATER PERMIT FOR POWER GENERATION</h6>
+                <ul class="4k:text-2xl list-disc list-inside mb-10 ml-[19px]">
+                    <li>
+                        Proof of land ownership or legal right to use the property where the water
+                        source is situated
+                    </li>
+                    <li>
+                        Certificate of Registration from DTI, CDA, or SEC, with Articles of
+                        Incorporation and Corporate Secretary Certification on capital structure
+                    </li>
+                    <li>
+                        Vicinity Map/Location Plan (scale 1:10,000 or 1:50,000) showing the exact
+                        location of the point of diversion
+                    </li>
+                    <li>
+                        Brief Description of the project, including how water will be used, the
+                        amount needed, and the power to be generated
+                    </li>
+                    <li>
+                        Environmental Compliance Certificate (ECC) or Certificate of Non-Coverage
+                        (CNC) from DENR-Regional Office
+                    </li>
+                    <li>Certificate of Registration from DOE and Certificate of Indorsement</li>
+                    <li>Other documents that may be required by the Board</li>
+                </ul>
+
+                <h6 class="mb-2 uppercase">WATER PERMIT FOR INDUSTRIAL USE</h6>
+                <ul class="4k:text-2xl list-disc list-inside mb-10 ml-[19px]">
+                    <li>
+                        Proof of land ownership or legal right to use the property where the water
+                        source is situated
+                    </li>
+                    <li>
+                        Certificate of Registration from DTI, CDA, or SEC, with Articles of
+                        Incorporation and Corporate Secretary Certification on capital structure
+                    </li>
+                    <li>
+                        Vicinity Map/Location Plan (scale 1:10,000 or 1:50,000) showing the exact
+                        location of the point of diversion
+                    </li>
+                    <li>
+                        Brief Description of the project, including how water will be used and the
+                        amount needed
+                    </li>
+                    <li>Well Drilling Data (for existing groundwater sources)</li>
+                    <li>
+                        Environmental Compliance Certificate (ECC) or Certificate of Non-Coverage
+                        (CNC) from DENR-Regional Office
+                    </li>
+                    <li>Other documents that may be required by the Board</li>
+                </ul>
+
+                <h6 class="mb-2 uppercase">WATER PERMIT FOR FISHERIES</h6>
+                <ul class="4k:text-2xl list-disc list-inside mb-10 ml-[19px]">
+                    <li>
+                        Proof of land ownership or legal right to use the property where the water
+                        source is situated
+                    </li>
+                    <li>
+                        Certificate of Registration from DTI, CDA, or SEC, with Articles of
+                        Incorporation and Corporate Secretary Certification on capital structure
+                    </li>
+                    <li>
+                        Vicinity Map/Location Plan (scale 1:10,000 or 1:50,000) showing the exact
+                        location of the point of diversion
+                    </li>
+                    <li>
+                        Brief Description of the project, including how water will be used and the
+                        amount needed
+                    </li>
+                    <li>Well Drilling Data (for existing groundwater sources)</li>
+                    <li>
+                        Environmental Compliance Certificate (ECC) or Certificate of Non-Coverage
+                        (CNC) from DENR-Regional Office
+                    </li>
+                    <li>
+                        Clearance from existing dam/reservoir operated by NIA, NPC, and other
+                        government entities (for fisheries located upstream)
+                    </li>
+                    <li>Other documents that may be required by the Board</li>
+                </ul>
+
+                <h6 class="mb-2 uppercase">
+                    WATER PERMIT FOR OTHER USES (RECREATION, COMMERCIAL, MEMORIAL PARK, ETC.)
+                </h6>
+                <ul class="4k:text-2xl list-disc list-inside mb-10 ml-[19px]">
+                    <li>
+                        Proof of land ownership or legal right to use the property where the water
+                        source is situated
+                    </li>
+                    <li>
+                        Certificate of Registration from DTI, CDA, or SEC, with Articles of
+                        Incorporation and Corporate Secretary Certification on capital structure
+                    </li>
+                    <li>
+                        Vicinity Map/Location Plan (scale 1:10,000 or 1:50,000) showing the exact
+                        location of the point of diversion
+                    </li>
+                    <li>
+                        Brief Description of the project, including how water will be used and the
+                        amount needed
+                    </li>
+                    <li>Well Drilling Data (for existing groundwater sources)</li>
+                    <li>
+                        Certificate of Potability (including Physical and Chemical Analysis of
+                        water), if applicable
+                    </li>
+                    <li>
+                        Environmental Compliance Certificate (ECC) or Certificate of Non-Coverage
+                        (CNC) from DENR-Regional Office
+                    </li>
+                    <li>Other documents that may be required by the Board</li>
+                </ul>
+
+                <h6 class="mb-2 uppercase">WATER PERMIT FOR LIVESTOCK RAISING</h6>
+                <ul class="4k:text-2xl list-disc list-inside mb-10 ml-[19px]">
+                    <li>
+                        Proof of land ownership or legal right to use the property where the water
+                        source is situated
+                    </li>
+                    <li>
+                        Certificate of Registration from DTI, CDA, or SEC, with Articles of
+                        Incorporation and Corporate Secretary Certification on capital structure
+                    </li>
+                    <li>
+                        Vicinity Map/Location Plan (scale 1:10,000 or 1:50,000) showing the exact
+                        location of the point of diversion
+                    </li>
+                    <li>Well Drilling Data (for existing groundwater sources)</li>
+                    <li>
+                        Brief Description of the project, including how water will be used and the
+                        amount needed
+                    </li>
+                    <li>
+                        Environmental Compliance Certificate (ECC) or Certificate of Non-Coverage
+                        (CNC) from DENR-Regional Office
+                    </li>
+                    <li>Other documents that may be required by the Board</li>
+                </ul>
+
+                <h6 class="mb-2 uppercase">OTHER REQUIREMENTS</h6>
+                <ul class="4k:text-2xl list-disc list-inside mb-10 ml-[19px]">
+                    <li>
+                        For Well Drilling: The application must include the name of a duly
+                        registered well driller.
+                    </li>
+                    <li>
+                        For Transfer of a Water Permit: A verified petition stating the reasons for
+                        the transfer, with the attached contract or agreement.
+                    </li>
+                    <li>
+                        For Lease of a Water Right: A verified petition accompanied by a duly
+                        executed contract of lease.
+                    </li>
+                    <li>
+                        For Bulk Water Supply: An agreement, MOA, or Joint Venture Agreement between
+                        the supplier and the buyer, specifying how the water will be used
+                    </li>
+                </ul>
+
+                <div class="flex flex-col gap-3 items-start justify-center">
+                    <NuxtLink
+                        to="#"
+                        class="flex-1 4k:text-2xl bg-[#547326] transition-all duration-200 text-white py-2 px-5 rounded"
+                    >
+                        Download PDF
+                    </NuxtLink>
+                    <NuxtLink
+                        to="#top"
+                        class="flex-1 4k:text-2xl border-2 border-[#547326] transition-all duration-200 text-[#547326] py-2 px-5 rounded"
+                    >
+                        Back to top
+                    </NuxtLink>
+                </div>
+            </section>
+        </div>
+    </section>
+</template>
+
+<script setup>
+useHead({
+    title: "National Water Resources Board (NWRB) Permit - Envi-Comm Corporation",
+});
+</script>
+
+<style lang="scss" scoped></style>
