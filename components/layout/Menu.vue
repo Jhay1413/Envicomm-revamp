844c04a410537abc3a19c79a322d0f5a456f13b4
fatmatawfeek <fatmatawfeek97@gmail.com>
2025-09-01 10:01:42 +0300

    create resources page

--- components/layout/Menu.vue
+++ components/layout/Menu.vue
@@ -1,74 +1,50 @@
 <template>
-    <ul class="navbar flex flex-col justify-center gap-[34px] lg:flex-row">
+    <ul class="navbar text-white flex flex-col justify-center gap-[34px] lg:flex-row">
         <li class="flex items-center group">
             <NuxtLink
-                class="text-white hover:text-green-900 uppercase text-clamp font-inter menu-link lg:text-heading-6 mr-[7px]"
+                class="hover:text-green-900 uppercase text-clamp font-inter menu-link lg:text-heading-6 mr-[7px]"
                 to="/"
+                active-class="text-green-900 font-bold"
                 >Home</NuxtLink
             >
         </li>
         <li class="flex items-center group">
             <NuxtLink
-                class="text-white hover:text-green-900 uppercase text-clamp font-inter menu-link lg:text-heading-6 mr-[7px]"
+                class="hover:text-green-900 uppercase text-clamp font-inter menu-link lg:text-heading-6 mr-[7px]"
                 to="/coming-soon"
+                active-class="text-green-900 font-bold"
                 >About</NuxtLink
             >
         </li>
         <li class="flex items-center group">
             <NuxtLink
-                class="text-white hover:text-green-900 uppercase text-clamp font-inter menu-link lg:text-heading-6 mr-[7px]"
+                class="hover:text-green-900 uppercase text-clamp font-inter menu-link lg:text-heading-6 mr-[7px]"
                 to="/coming-soon"
+                active-class="text-green-900 font-bold"
                 >Services</NuxtLink
             >
-            <!-- <i><img class="icon-caret group-hover:filter-green" src="/assets/images/icons/icon-caret.svg" alt="caret" /></i> -->
-            <!-- <ul class="menu-child opacity-0 bg-white top-full z-50 py-2 grid menu-shadow -translate-x-6 translate-y-8 transition-all duration-200 pointer-events-none group-hover:pointer-events-auto lg:absolute rounded-[4px] group-hover:grid group-hover:opacity-100 before:content-[''] before:block before:absolute before:w-full before:h-12 before:top-[-35px] before:left-0 grid-cols-1 w-[185px]">
-                <li class="menu-child-item  group transition-all duration-200 py-[10px] px-[22px] hover:filter-green hover:pl-[25px] hover:opacity-100" data-menu="sv1">
-                    <NuxtLink class="flex items-center text-[14px]" to="/what-we-do-1"><img class="opacity-40 w-[12px] h-[12px] mr-[8px] -translate-y-[1px]" src="/assets/images/icons/icon-gem.svg" alt="" /><span class="whitespace-nowrap">Services 01</span></NuxtLink>
-                </li>
-                <li class="menu-child-item  group transition-all duration-200 py-[10px] px-[22px] hover:filter-green hover:pl-[25px] hover:opacity-100" data-menu="sv2">
-                    <NuxtLink class="flex items-center text-[14px]" to="/what-we-do-2"><img class="opacity-40 w-[12px] h-[12px] mr-[8px] -translate-y-[1px]" src="/assets/images/icons/icon-gem.svg" alt="" /><span class="whitespace-nowrap">Services 02</span></NuxtLink>
-                </li>
-                <li class="hr px-[22px]"><span class="block bg-gray-100 w-full h-[1px] my-[5px]"></span></li>
-                <li class="menu-child-item  group transition-all duration-200 py-[10px] px-[22px] hover:filter-green hover:pl-[25px] hover:opacity-100" data-menu="p1">
-                    <NuxtLink class="flex items-center text-[14px]" to="/pricing-1"><img class="opacity-40 w-[12px] h-[12px] mr-[8px] -translate-y-[1px]" src="/assets/images/icons/icon-dataclamp.svg" alt="" /><span class="whitespace-nowrap">Pricing 01</span></NuxtLink>
-                </li>
-                <li class="menu-child-item  group transition-all duration-200 py-[10px] px-[22px] hover:filter-green hover:pl-[25px] hover:opacity-100" data-menu="p2">
-                    <NuxtLink class="flex items-center text-[14px]" to="/pricing-2"><img class="opacity-40 w-[12px] h-[12px] mr-[8px] -translate-y-[1px]" src="/assets/images/icons/icon-dataclamp.svg" alt="" /><span class="whitespace-nowrap">Pricing 02</span></NuxtLink>
-                </li>
-                <li class="hr px-[22px]"><span class="block bg-gray-100 w-full h-[1px] my-[5px]"></span></li>
-                <li class="menu-child-item  group transition-all duration-200 py-[10px] px-[22px] hover:filter-green hover:pl-[25px] hover:opacity-100" data-menu="faqs1">
-                    <NuxtLink class="flex items-center text-[14px]" to="/faqs-1"><img class="opacity-40 w-[12px] h-[12px] mr-[8px] -translate-y-[1px]" src="/assets/images/icons/icon-headset.svg" alt="" /><span class="whitespace-nowrap">FAQS 01</span></NuxtLink>
-                </li>
-                <li class="menu-child-item  group transition-all duration-200 py-[10px] px-[22px] hover:filter-green hover:pl-[25px] hover:opacity-100" data-menu="faqs2">
-                    <NuxtLink class="flex items-center text-[14px]" to="/faqs-2"><img class="opacity-40 w-[12px] h-[12px] mr-[8px] -translate-y-[1px]" src="/assets/images/icons/icon-headset.svg" alt="" /><span class="whitespace-nowrap">FAQS 02</span></NuxtLink>
-                </li>
-                <li class="hr px-[22px]"><span class="block bg-gray-100 w-full h-[1px] my-[5px]"></span></li>
-                <li class="menu-child-item  group transition-all duration-200 py-[10px] px-[22px] hover:filter-green hover:pl-[25px] hover:opacity-100" data-menu="career">
-                    <NuxtLink class="flex items-center text-[14px]" to="/career"><img class="opacity-40 w-[12px] h-[12px] mr-[8px] -translate-y-[1px]" src="/assets/images/icons/icon-briefcase.svg" alt="" /><span class="whitespace-nowrap">Career</span></NuxtLink>
-                </li>
-                <li class="menu-child-item  group transition-all duration-200 py-[10px] px-[22px] hover:filter-green hover:pl-[25px] hover:opacity-100" data-menu="career-details">
-                    <NuxtLink class="flex items-center text-[14px]" to="/career-details"><img class="opacity-40 w-[12px] h-[12px] mr-[8px] -translate-y-[1px]" src="/assets/images/icons/icon-briefcase.svg" alt="" /><span class="whitespace-nowrap">Career Detials</span></NuxtLink>
-                </li>
-            </ul> -->
         </li>
         <li class="flex items-center group">
             <NuxtLink
-                class="text-white hover:text-green-900 uppercase text-clamp font-inter menu-link lg:text-heading-6 mr-[7px]"
+                class="hover:text-green-900 uppercase text-clamp font-inter menu-link lg:text-heading-6 mr-[7px]"
                 to="/coming-soon"
+                active-class="text-green-900 font-bold"
                 >Careers</NuxtLink
             >
         </li>
         <li class="flex items-center group">
             <NuxtLink
-                class="text-white hover:text-green-900 uppercase text-clamp font-inter menu-link lg:text-heading-6 mr-[7px]"
+                class="hover:text-green-900 uppercase text-clamp font-inter menu-link lg:text-heading-6 mr-[7px]"
                 to="/coming-soon"
+                active-class="text-green-900 font-bold"
                 >Contact</NuxtLink
             >
         </li>
         <li class="flex items-center group">
             <NuxtLink
-                class="text-white hover:text-green-900 uppercase text-clamp font-inter menu-link lg:text-heading-6 mr-[7px]"
-                to="/coming-soon"
+                class="hover:text-green-900 uppercase text-clamp font-inter menu-link lg:text-heading-6 mr-[7px]"
+                to="/resources"
+                active-class="text-green-900 font-bold"
                 >Resources</NuxtLink
             >
         </li>
---
+++ pages/resources.vue
@@ -0,0 +1,103 @@
+<template>
+    <!-- hero section  -->
+    <section
+        class="bg-[url('/assets/images/main-imgs/resources.jpg')] bg-cover bg-left md:bg-top min-h-dvh py-20 w-full px-10 md:px-20 flex items-center"
+        id="top"
+    >
+        <div class="md:max-w-[40%] text-black">
+            <!-- text 1 -->
+            <h1 class="text-2xl md:text-5xl 4k:text-6xl mb-5">Resources</h1>
+            <p class="text-lg 4k:text-3xl">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis lorem tortor, vehicula
+                consectetur neque at, pharetra dictum lorem. Vivamus molestie facilisis turpis, ac
+                condimentum dolor luctus a. Nulla ut tincidunt nisi, ultricies sollicitudin quam.
+                Nunc urna justo, finibus quis vestibulum eget, blandit a odio. Morbi tempus elit ac
+                felis hendrerit, a bibendum arcu aliquet. In faucibus tincidunt nibh, nec
+                sollicitudin metus. Aenean lacinia ac elit sit amet sagittis. Aliquam sit amet mi
+                sed nisl congue consequat.
+            </p>
+        </div>
+    </section>
+    <!-- body section  -->
+    <section class="bg-[#EBF5D7] py-10 md:py-20">
+        <div class="flex flex-col md:flex-row justify-between items-center gap-5">
+            <div class="md:basis-1/2 md:ml-20 w-full px-5 md:px-0">
+                <NuxtLink
+                    to="/what-we-do/denr-application-and-permitting"
+                    class="bg-[#547326] hover:bg-[#4c6822] text-white rounded-lg py-3 px-5 w-full block mb-3"
+                    >DENR: Application & Permitting</NuxtLink
+                >
+                <NuxtLink
+                    to="/what-we-do/denr-reporting"
+                    class="bg-[#547326] hover:bg-[#4c6822] text-white rounded-lg py-3 px-5 w-full block mb-3"
+                    >DENR: Reporting</NuxtLink
+                >
+                <NuxtLink
+                    to="/what-we-do/denr-mapping-and-planning"
+                    class="bg-[#547326] hover:bg-[#4c6822] text-white rounded-lg py-3 px-5 w-full block mb-3"
+                    >DENR: Mapping & Planning</NuxtLink
+                >
+                <NuxtLink
+                    to="/what-we-do/stps-and-wtps"
+                    class="bg-[#547326] hover:bg-[#4c6822] text-white rounded-lg py-3 px-5 w-full block mb-3"
+                    >STPs & WTPs</NuxtLink
+                >
+                <NuxtLink
+                    to="/what-we-do/chemical-stp-and-ro"
+                    class="bg-[#547326] hover:bg-[#4c6822] text-white rounded-lg py-3 px-5 w-full block mb-3"
+                    >Chemical: STP & RO</NuxtLink
+                >
+                <NuxtLink
+                    to="/what-we-do/renewable-energy-product-and-technology"
+                    class="bg-[#547326] hover:bg-[#4c6822] text-white rounded-lg py-3 px-5 w-full block mb-3"
+                    >Renewable Energy Product & Technology</NuxtLink
+                >
+                <NuxtLink
+                    to="/what-we-do/hauling-and-septage"
+                    class="bg-[#547326] hover:bg-[#4c6822] text-white rounded-lg py-3 px-5 w-full block mb-3"
+                    >Hauling & Septage</NuxtLink
+                >
+                <NuxtLink
+                    to="/what-we-do/saas-development"
+                    class="bg-[#547326] hover:bg-[#4c6822] text-white rounded-lg py-3 px-5 w-full block mb-3"
+                    >Saas Development</NuxtLink
+                >
+                <NuxtLink
+                    to="/what-we-do/pco-academy"
+                    class="bg-[#547326] hover:bg-[#4c6822] text-white rounded-lg py-3 px-5 w-full block mb-3"
+                    >PCO Academy</NuxtLink
+                >
+                <NuxtLink
+                    to="/what-we-do/specialized-data-collection-air-quality"
+                    class="bg-[#547326] hover:bg-[#4c6822] text-white rounded-lg py-3 px-5 w-full block mb-3"
+                    >Specialized Data Collection: Air Quality</NuxtLink
+                >
+                <NuxtLink
+                    to="/what-we-do/denr-professional-and-specialized-authority"
+                    class="bg-[#547326] hover:bg-[#4c6822] text-white rounded-lg py-3 px-5 w-full block mb-3"
+                    >DENR: Professional & Specialized Authority</NuxtLink
+                >
+                <NuxtLink
+                    to="/what-we-do/autoclave-and-pyrolysis-technology"
+                    class="bg-[#547326] hover:bg-[#4c6822] text-white rounded-lg py-3 px-5 w-full block mb-3"
+                    >Autoclave and Pyrolysis Technology</NuxtLink
+                >
+            </div>
+            <div>
+                <img
+                    src="/assets/images/main-imgs/resources-bg.png"
+                    class="w-full opacity-50"
+                    alt=""
+                />
+            </div>
+        </div>
+    </section>
+</template>
+
+<script>
+useHead({
+    title: "Resources - Envi-Comm Corporation",
+});
+</script>
+
+<style lang="scss" scoped></style>
