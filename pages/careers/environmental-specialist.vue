1de0f8627af808dca25e09ebaad34b82e785b7ba
fatmatawfeek <fatmatawfeek97@gmail.com>
2025-09-03 19:57:54 +0300

    create Environmental Specialist page

---
+++ pages/careers/environmental-specialist.vue
@@ -0,0 +1,121 @@
+<template>
+    <section
+        class="md:bg-[url('/assets/images/main-imgs/careers/environmental-specialist.jpg')] bg-cover bg-center pt-20 w-full"
+    >
+        <div class="px-5 md:px-20">
+            <h1 class="text-3xl md:text-5xl md:text-white font-semibold uppercase 4k:text-6xl">
+                Environmental Specialist
+            </h1>
+            <div
+                class="flex flex-col md:flex-row justify-start items-center gap-10 4k:gap-40 py-20 4k:py-40"
+            >
+                <div class="md:max-w-[63%] 4k:max-w-[45%] text-[13px] 4k:text-2xl">
+                    <p class="mb-2">
+                        <span class="font-semibold">Department: </span>Environmental Services
+                    </p>
+                    <p class="mb-2"><span class="font-semibold">Location: </span>Cebu City</p>
+                    <p class="mb-2">
+                        <span class="font-semibold">Category: </span>Environmental Management
+                    </p>
+                    <p class="mb-2"><span class="font-semibold">Job Type: </span>Full Time</p>
+                    <p class="mb-2">
+                        <span class="font-semibold">Line Manager / Supervisor: </span>Environmental
+                        Services Manager
+                    </p>
+                    <p class="mb-2">
+                        <span class="font-semibold">Key Relationships: </span>Environmental Services
+                        Manager, Operations Team, Government Regulatory Agencies
+                    </p>
+
+                    <h3 class="font-semibold text-xl mb-2 mt-5">Job Description</h3>
+                    <p class="mb-2">
+                        We are looking for a skilled and dedicated Environmental Specialist to join
+                        Envi-comm Corporation. The ideal candidate will play a critical role in
+                        ensuring our environmental programs are compliant with all applicable
+                        environmental laws, regulations, and standards.
+                    </p>
+
+                    <h3 class="font-semibold text-xl mb-2 mt-5">Key Responsibilities</h3>
+                    <ul class="list-disc list-inside mb-2 ml-[15px]">
+                        <li class="mb-2">
+                            Monitor and evaluate environmental performance across operations.
+                        </li>
+                        <li class="mb-2">
+                            Develop and implement environmental policies and practices.
+                        </li>
+                        <li class="mb-2">
+                            Ensure compliance with environmental legislation and keep up to date
+                            with changes in environmental laws.
+                        </li>
+                        <li class="mb-2">
+                            Conduct site inspections and audits to assess environmental impact and
+                            compliance.
+                        </li>
+                        <li class="mb-2">
+                            Identify environmental risks and propose mitigation strategies.
+                        </li>
+                        <li class="mb-2">
+                            Prepare environmental reports and present findings to management and
+                            regulatory bodies.
+                        </li>
+                        <li class="mb-2">
+                            Collaborate with internal departments and government agencies on
+                            environmental issues.
+                        </li>
+                        <li class="mb-2">
+                            Handle confidential and sensitive information related to environmental
+                            incidents, permits, and regulatory correspondence.
+                        </li>
+                    </ul>
+
+                    <h3 class="font-semibold text-xl mb-2 mt-5">Skills and Qualifications</h3>
+                    <ul class="4k:text-2xl list-disc list-inside mb-2 ml-[15px]">
+                        <li class="mb-2">
+                            In-depth knowledge of environmental laws, regulations, and standards,
+                            including DENR, EMB, and other relevant local and national regulatory
+                            bodies.
+                        </li>
+                        <li class="mb-2">
+                            Strong understanding of environmental impact assessment (EIA) procedures
+                            and reporting.
+                        </li>
+                        <li class="mb-2">
+                            Excellent interpersonal skills with the ability to educate and influence
+                            staff on environmental practices.
+                        </li>
+                        <li class="mb-2">Strong analytical and problem-solving skills.</li>
+                        <li class="mb-2">
+                            Proficient in data analysis, environmental management software, and
+                            Microsoft Office Suite.
+                        </li>
+                        <li class="mb-2">Effective written and verbal communication skills.</li>
+                        <li class="mb-2">
+                            Experience in preparing documentation for environmental permits and
+                            reports.
+                        </li>
+                        <li class="mb-2">
+                            Bachelor’s/College Degree in Environmental Science, Environmental
+                            Engineering, or related field.
+                        </li>
+                        <li class="mb-2">
+                            Professional License (e.g., Registered Environmental Planner or
+                            equivalent) is an advantage.
+                        </li>
+                    </ul>
+                </div>
+
+                <div class="w-full md:w-auto">
+                    <SectionsCareerApplyForm />
+                </div>
+            </div>
+        </div>
+    </section>
+</template>
+
+<script setup>
+useHead({
+    title: "Environmental Specialist - Envi-Comm Corporation",
+});
+</script>
+
+<style lang="scss" scoped></style>
